c6a989f9faab5ae701ddcc93a8e6a284e2af6251
Wylie Kuhn <86081235+WylieKuhn@users.noreply.github.com>
2026-03-13 17:17:30 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+import time
+
+sys.set_int_max_str_digits(1_000_000)
+
+number = int(input("Enter a number: "))
+
+num = (2**number) + 1
+
+step = 0
+start = time.time()
+three_x = 0
+divide_two = 0
+
+while num > 1:
+    if num & 1:
+        num = ((num << 1) + num) + 1
+        three_x += 1
+    else:
+        num = num >> 1
+        divide_two += 1
+      
+    step += 1
+        
+
+print(f"Sequence Length: {step:,}")
+print (f"3x+1 steps: {three_x:,}")
+print(f"Divide by 2 steps: {divide_two:,}")
+print("Time taken:", time.time() - start)
